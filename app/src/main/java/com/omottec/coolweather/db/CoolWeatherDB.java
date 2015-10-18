1b6bc229201dd4658baafe93740a48325d6a2b54
omottec <omottec@gmail.com>
2015-10-18 19:44:01 +0800

    demo complete

--- app/src/main/java/com/omottec/coolweather/db/CoolWeatherDB.java
+++ app/src/main/java/com/omottec/coolweather/db/CoolWeatherDB.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.coolweather.app.model.City;
-import com.coolweather.app.model.County;
-import com.coolweather.app.model.Province;
+import com.omottec.coolweather.model.City;
+import com.omottec.coolweather.model.County;
+import com.omottec.coolweather.model.Province;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ app/src/main/java/com/omottec/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,9 @@
+package com.omottec.coolweather.util;
+
+public interface HttpCallbackListener {
+
+	void onFinish(String response);
+
+	void onError(Exception e);
+
+}
